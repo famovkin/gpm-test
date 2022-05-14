07d2da753d52cc5ab932123c22737e5e91461c12
Tolya Fam <famovkin@yandex.ru>
2022-05-14 21:26:27 +0300

    create project basic structure

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Dashboard</h1>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
